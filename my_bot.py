2b74f14107bab419a97c15fdbdfb6b2f8c4e32f3
potterdev <gonchar.vladimir@yahoo.com>
2019-04-14 22:44:23 +0300

    update version my_bot.py

--- my_bot.py
+++ my_bot.py
@@ -19,7 +19,7 @@ import threading
 
 app = Flask(__name__)
 viber = Api(BotConfiguration(
-    name='PythonSampleBot',
+    name='helpegisso',
     avatar='http://viber.com/avatar.jpg',
     auth_token='496bdc821627d6e3-89019a2a752a3f08-58f225f6ba43594'
 ))
