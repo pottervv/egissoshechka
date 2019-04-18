0e9baa8a969a501bf43006392be05e3eb5f30473
potterdev <gonchar.vladimir@yahoo.com>
2019-04-18 21:52:44 +0300

    использование aioviber

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn my_bot:app
+web: python helpegisso.py
---
+++ helpegisso.py
@@ -0,0 +1,31 @@
+import logging
+
+from aioviber.bot import Bot
+from aioviber.chat import Chat
+from viberbot.api.viber_requests import ViberSubscribedRequest
+
+logger = logging.getLogger(__name__)
+
+bot = Bot(
+    name='helpegisso',
+    avatar="http://viber.com/avatar.jpg",
+    auth_token='496bdc821627d6e3-89019a2a752a3f08-58f225f6ba43594',  # Public account auth token
+    host="my.host.com",  # should be available from wide area network
+    port=80,
+    webhook="https://egissoshechka.herokuapp.com",  # Webhook url
+)
+
+@bot.command('ping')
+async def ping(chat: Chat, matched):
+    await chat.send_text('pong')
+
+@bot.event_handler('subscribed')
+async def user_subscribed(chat: Chat, request: ViberSubscribedRequest):
+    await chat.send_text('Welcome')
+
+@bot.message_handler('sticker')
+async def sticker(chat: Chat):
+    await chat.send_sticker(5900)
+
+if __name__ == '__main__':  # pragma: no branch
+    bot.run()  # pragma: no cover
