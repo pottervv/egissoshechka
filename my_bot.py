670b2fc48baaa9ac4882cd7af437cfe9234d4394
potterv <gonchar.vladimir@hotmail.com>
2019-04-18 16:49:05 +0300

    Обновление my_bot.py, вывод сообщения.

--- my_bot.py
+++ my_bot.py
@@ -110,7 +110,7 @@ def incoming():
     viber_request = viber.parse_request(request.get_data())
 
     if isinstance(viber_request, ViberConversationStartedRequest):
-        viber.send_messages(viber_request.user, [ TextMessage(text="Welcome!")])
+        viber.send_messages(viber_request.user.id, [ TextMessage(text="Welcome!")])
         logger.debug(" viber_request.get_user().get_id()-{0}".format(viber_request.get_user().get_id()))
 
     elif isinstance(viber_request, ViberMessageRequest):
@@ -119,12 +119,12 @@ def incoming():
 
     elif isinstance(viber_request, ViberSubscribedRequest):
              #viber.send_messages(viber_request.get_user().get_id(), [TextMessage(viber_request.get_event_type())])
-             viber.send_messages(viber_request.user,
+             viber.send_messages(viber_request.user.id,
                                  [TextMessage(text="Здравствуйте! Вас приветствует бот helpegisso. Все о мире ЕГИССО")])
              logger.debug("1_viber_request.get_event_type():{0}".format(viber_request.get_event_type()))
 
     elif isinstance(viber_request, ViberUnsubscribedRequest):
-        viber.send_messages(viber_request.user_id, [TextMessage(text="Вы отписались!")])
+        viber.send_messages(viber_request.user.id, [TextMessage(text="Вы отписались!")])
         logger.debug("1_viber_request.get_event_type():{0}".format(viber_request.get_event_type()))
 
     elif isinstance(viber_request, ViberFailedRequest):
