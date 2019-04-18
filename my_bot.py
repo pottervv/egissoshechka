cf96ef323060257830edeac653676cce57621a13
potterdev <gonchar.vladimir@yahoo.com>
2019-04-18 20:43:06 +0300

    обновление my_bot.py  " благодарность за подписку"

--- my_bot.py
+++ my_bot.py
@@ -111,10 +111,10 @@ def incoming():
     viber_request = viber.parse_request(request.get_data())
 
     if isinstance(viber_request, ViberConversationStartedRequest):
-        viber.send_messages(viber_request.user.id, [ TextMessage(text="Здравствуйте! Вас приветствует бот helpegisso. Все о мире ЕГИССО")])
+        viber.send_messages(viber_request.user.id, [TextMessage(text="Здравствуйте! Вас приветствует бот helpegisso. Все о мире ЕГИССО")])
         logger.debug(" viber_request.get_user().get_id()-{0}".format(viber_request.user.id))
 
-    elif isinstance(viber_request, ViberSubscribedRequest):
+    if isinstance(viber_request, ViberSubscribedRequest):
              #viber.send_messages(viber_request.user.id, viber_request.get_event_type())
              viber.send_messages(viber_request.user_id,[TextMessage(text="Спасибо за подписку!")])
              logger.debug("_viber_request.get_event_type():{0}".format(viber_request.get_event_type()))
@@ -127,9 +127,10 @@ def incoming():
     elif isinstance(viber_request, ViberFailedRequest):
         logger.warning("client failed receiving message. failure: {0}".format(viber_request))
 
+    """ 
     elif isinstance(viber_request, ViberMessageRequest):
        viber.send_messages(to=viber_request.sender.id,messages=[TextMessage(text="С Вами так интересно")])
-
+    """
 
     return Response(status=200)
 
