2d8d567ffe2e60a81cff70439f3cc9cc39bb195a
potterdev <gonchar.vladimir@yahoo.com>
2019-04-22 22:05:27 +0300

    добавляем клавиатуру

--- my_bot.py
+++ my_bot.py
@@ -59,7 +59,7 @@ def incoming():
             "TextSize": "regular"
         }]
     }
-    keyboard = str(keyboarddate)
+    keyboard = eval(str(json.dumps(keyboarddate)))
 
     logger.debug("received request. post data: {0}".format(request.get_data()))
     # every viber message is signed, you can verify the signature using this method
