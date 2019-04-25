58bb2c8819fc386785ff37d64c4263398b0c22f7
potterv <gonchar.vladimir@hotmail.com>
2019-04-25 09:35:24 +0300

    add keyboard

--- my_bot.py
+++ my_bot.py
@@ -42,7 +42,7 @@ def set_webhook(viber):
 
 @app.route('/', methods=['POST'])
 def incoming():
-    keyboarddate = {
+    keyboarddate = """{
         "DefaultHeight": True,
         "BgColor": "#FFFFFF",
         "Buttons": [{
@@ -59,8 +59,8 @@ def incoming():
             "TextOpacity": 60,
             "TextSize": "regular"
         }]
-    }
-    keyboard = json.dumps(keyboarddate)
+    }"""
+    keyboard = json.loads(keyboarddate)
     keyb={"DefaultHeight": True}
 
     logger.debug("received request. post data: {0}".format(request.get_data()))
@@ -97,9 +97,9 @@ def incoming():
         message_stiker = StickerMessage(sticker_id=40100);
         message_key = KeyboardMessage(tracking_data=json.dumps({"text":"purga"}), keyboard=keyboard)
         logger.debug("keyboard:{0}".format(keyboard))
-        #text_m=TextMessage(text="Для начинающих")
+        text_m=TextMessage(text="Для начинающих",keyboard=keyboard)
         account_info = TextMessage(text=str(viber.get_account_info()))
-        viber.send_messages(to=viber_request.sender.id, messages=[account_info])
+        viber.send_messages(to=viber_request.sender.id, messages=[text_m])
 
     return Response(status=200)
 
